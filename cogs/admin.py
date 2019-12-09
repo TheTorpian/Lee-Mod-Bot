36e52b002193c5dfa6d57b60ad7987521379cff6
TheTorpian <alexxgabriel18@gmail.com>
2019-12-09 22:29:28 +0100

    added cogs

---
+++ cogs/admin.py
@@ -0,0 +1,34 @@
+import discord.utils
+from discord.ext import commands
+from tokenfile import Vars
+
+
+class AdminCog(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	@commands.Cog.listener()  # listener, checks on member join, assigns default role
+	async def on_member_join(self, member):
+		role = discord.utils.get(member.guild.roles, name='baby chicks')
+		if role not in member.roles:
+			await member.add_roles(role)
+
+	@commands.command(pass_context=True)  # just like say but in a specified channel
+	@commands.check(Vars.user_is_me)
+	async def says(self, ctx, ch, *args):
+		channel = self.bot.get_channel(int(ch))
+		await channel.send(' '.join(args))
+
+	@commands.command()  # adds letmeknow role
+	async def letmeknow(self, ctx):
+		role = discord.utils.get(ctx.author.guild.roles, name='letmeknow')
+		if role in ctx.author.roles:
+			await ctx.send('You already have the role...')
+		else:
+			await ctx.author.add_roles(role)
+			await ctx.send('Role added!')
+
+
+def setup(bot):
+	bot.add_cog(AdminCog(bot))
---
+++ cogs/test.py
@@ -0,0 +1,29 @@
+import discord.utils
+from discord.ext import commands
+
+
+class TestCog(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	@commands.has_role('Security')
+	async def test_role(self, ctx):
+		role = discord.utils.get(ctx.guild.roles, name='Security')
+		if role in ctx.author.roles:
+			await ctx.send(f'You already have the role {role.name}')
+		else:
+			await ctx.send('You don\'t have the role')
+
+	@commands.command()
+	async def test_assign(self, ctx):
+		role = discord.utils.get(ctx.author.guild.roles, name='test')
+		await ctx.author.add_roles(role)
+
+	@commands.command()
+	async def test(self, ctx):
+		await ctx.send('I work')
+
+
+def setup(bot):
+	bot.add_cog(TestCog(bot))
