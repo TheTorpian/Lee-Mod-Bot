46edd496adef5c9fe14e36a871a91457b1941213
TheTorpian <alexxgabriel18@gmail.com>
2019-12-12 14:06:57 +0100

    added the bot's tag

--- tokenfile.example.py
+++ tokenfile.example.py
@@ -3,6 +3,7 @@ class Vars:
     TOKEN = ''  # bot app link
     torp_tag = [249550049564950530]  # big torpo's tag
     lee_tag = 384297196113231882
+    poleece_tag = 653600602764607498  # bot's tag
 
     def user_is_me(ctx):
         return ctx.message.author.id in Vars.torp_tag
