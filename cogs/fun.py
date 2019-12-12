fa1a79f8d81e8fbc2cfa739df0d0afdbbea116eb
TheTorpian <alexxgabriel18@gmail.com>
2019-12-12 14:06:13 +0100

    can't challenge the bot anymore

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,9 +1,8 @@
 import random
-
 import discord
 from discord.ext import commands
-
 from tokenfile import Vars
+
 
 class FunCog(commands.Cog):
     def __init__(self, bot):
@@ -11,25 +10,30 @@ class FunCog(commands.Cog):
 
     @commands.command()
     async def morning(self, ctx):
-        await ctx.send("https://tenor.com/view/leader-kim-jong-un-north-korea-gif-8893271")
+        await ctx.send('https://tenor.com/view/leader-kim-jong-un-north-korea-gif-8893271')
 
-    @commands.command()
+    @commands.command()  # challenge the tagged user
     async def challenge(self, ctx, tag: discord.Member):
         supreme_user = ctx.guild.get_member(Vars.lee_tag)
+        bot_user = ctx.guild.get_member(Vars.poleece_tag)
         outcome = random.randint(0, 1)
-        if tag == supreme_user:
-            await ctx.send("You don't challenge the Supreme Leader")
-        elif tag == ctx.author:
-            await ctx.send("You can't challenge yourself")
-        else:
-            if outcome:
-                await ctx.send(f'{tag.display_name} won!')
-                await ctx.send(f'{ctx.author.display_name} lost!')
-            else:
-                await ctx.send(f'{ctx.author.display_name} won!')
-                await ctx.send(f'{tag.display_name} lost!')
 
-    @commands.command()     # Happy birthday Lee
+        if tag == bot_user:  # PoLeece Bot never loses
+            await ctx.send('You don\'t challenge the Secret PoLeece.')
+        else:
+            if tag == supreme_user:
+                await ctx.send('You don\'t challenge the Supreme Leader')
+            elif tag == ctx.author:
+                await ctx.send('You can\'t challenge yourself')
+            else:
+                if outcome:
+                    await ctx.send(f'{tag.display_name} won!')
+                    await ctx.send(f'{ctx.author.display_name} lost!')
+                else:
+                    await ctx.send(f'{ctx.author.display_name} won!')
+                    await ctx.send(f'{tag.display_name} lost!')
+
+    @commands.command()  # Happy birthday Lee
     async def birthday(self, ctx):
         await ctx.send('Happy birthday Lee <:LeeBday:519424058652098560>')
 
