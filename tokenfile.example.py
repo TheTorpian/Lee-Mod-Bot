8ca8c5cecc6eaa5f163d4356ce2feacd8ab34ab0
TheTorpian <alexxgabriel18@gmail.com>
2019-12-17 16:37:21 +0100

    copy this to tokenfile.py

--- tokenfile.example.py
+++ tokenfile.example.py
@@ -1,9 +1,24 @@
+from discord.ext import commands
+
+
 class Vars:
     INVITE = ''  # bot invite link
     TOKEN = ''  # bot app link
-    torp_tag = [249550049564950530]  # big torpo's tag
+    torp_tag = 249550049564950530  # big torpo's tag
     lee_tag = 384297196113231882
     poleece_tag = 653600602764607498  # bot's tag
 
-    def user_is_me(ctx):
-        return ctx.message.author.id in Vars.torp_tag
+def user_is_torp():
+    def predicate(ctx):
+        return ctx.message.author.id == Vars.torp_tag
+    return commands.check(predicate)
+
+def check_ignore(ctx, channel):  # function that checks if channel id is in ignored_channels file (returns false if it is)
+    channel = str(channel)
+    with open('ignored_channels') as f:
+        if str(channel) in f.read():
+            f.close()
+            return False
+        else:
+            f.close()
+            return True
