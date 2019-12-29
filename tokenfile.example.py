a23855f8a4fc6772a196798d7193097afd8b16e8
TheTorpian <alexxgabriel18@gmail.com>
2019-12-29 14:19:53 +0100

    I gotta stop hard coding shit

--- tokenfile.example.py
+++ tokenfile.example.py
@@ -7,6 +7,8 @@ class Vars:
     torp_tag = 249550049564950530  # big torpo's tag
     lee_tag = 384297196113231882
     poleece_tag = 653600602764607498  # bot's tag
+    deleted_messages_channel = 660102195504742400  # deleted messages channel
+    ignored_channels = r''  # path to ignored_channels file
 
 
 def user_is_torp():
@@ -17,7 +19,7 @@ def user_is_torp():
 
 def check_ignore(ctx, channel):  # function that checks if channel id is in ignored_channels file (returns false if it is)
     channel = str(channel)
-    with open('ignored_channels') as f:
+    with open(Vars.ignored_channels) as f:
         if str(channel) in f.read():
             f.close()
             return False
