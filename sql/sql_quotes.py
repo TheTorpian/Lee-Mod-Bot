70919dc66b2904f9b0acfbf91e32a1a0312199d2
TheTorpian <alexxgabriel18@gmail.com>
2020-03-16 19:58:00 +0100

    am dumbass

--- sql/sql_quotes.py
+++ sql/sql_quotes.py
@@ -6,28 +6,28 @@ db = connection
 
 def add_quote(tag, quote, sv):  # adds quote to db
     cursor = tokenfile.get_cursor(connection)
-    query = 'INSERT INTO quote (quoted_tag, quote, server_id) VALUES (%s, %s, %s)'
+    query = 'INSERT INTO Quote (quoted_tag, quote, server_id) VALUES (%s, %s, %s)'
     cursor.execute(query, (tag, quote, sv))
     db.commit()
 
 
 def remove_quote(quote_nr):  # removes quote from db
     cursor = tokenfile.get_cursor(connection)
-    query = 'DELETE FROM quote WHERE id = %s'
+    query = 'DELETE FROM Quote WHERE id = %s'
     cursor.execute(query, (quote_nr,))
     db.commit()
 
 
 def update_quote(quote_nr, quote):  # alters quote
     cursor = tokenfile.get_cursor(connection)
-    query = 'UPDATE quote SET quote = %s WHERE id = %s'
+    query = 'UPDATE Quote SET quote = %s WHERE id = %s'
     cursor.execute(query, (quote, quote_nr))
     db.commit()
 
 
 def get_quote(sv):  # gets a random quote from current server
     cursor = tokenfile.get_cursor(connection)
-    query = 'SELECT id, quoted_tag, quote, tstamp FROM quote WHERE server_id = %s'
+    query = 'SELECT id, quoted_tag, quote, tstamp FROM Quote WHERE server_id = %s'
     cursor.execute(query, (sv,))
     quotes = cursor.fetchall()  # returns list of tuples, use double index to get actual values
     return quotes
